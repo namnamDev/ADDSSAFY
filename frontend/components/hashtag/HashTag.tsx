b453edccd661c1bb4c0c788ab5293ee230361b60
박종한 <jonghan1983@naver.com>
2021-10-22 17:55:42 +0900

    update HashTag.tsx

--- frontend/components/hashtag/HashTag.tsx
+++ frontend/components/hashtag/HashTag.tsx
@@ -1,21 +1,28 @@
-import React, { ReactElement } from "react";
+import React, { ReactElement, useEffect, useState } from "react";
 import Category from "../../dummy/json/categoryDump.json";
+import axios from "axios";
 interface Props {}
 
 function HashTag({}: Props): ReactElement {
+  // const [Category, setCategory] = useState();
+  const [index, setIndex] = useState(0);
+  // useEffect( () =>  {
+  //   const response = axios.get("https://jsonplaceholder.typicode.com/users");
+  //   setCategory(response.data);
+  // },[])
   return (
     <div>
       <div className="grid grid-cols-5">
-        <div>백엔드</div>
+        {/* <div onClick={setIndex(0)}>백엔드</div> */}
         <div>프론트엔드</div>
         <div>CI/CD</div>
         <div>4차 산업 기술</div>
         <div>기타</div>
       </div>
       <div>
-        {Category.map((value) => {
-          // return {value.title};
-        })}
+        {/* {Category.map((value) => {
+          return value.title;
+        })} */}
       </div>
     </div>
   );
