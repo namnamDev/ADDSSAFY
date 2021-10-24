f0d57dd0f229e89852dcb22d89fd354b983004ac
Yeongwoo <77479615+ChoYeongwoo0322@users.noreply.github.com>
2021-10-24 22:40:57 +0900

    데이터 leave, studying 구분해보기

--- FrontEnd/components/manage/UserList.tsx
+++ FrontEnd/components/manage/UserList.tsx
@@ -3,64 +3,89 @@ import { useRouter } from "next/router";
 import Image from "next/image";
 const people = [
   {
-    name: "Jane Cooper",
     userId: 1,
-    title: "Regional Paradigm Technician",
-    department: "Optimization",
-    role: "Admin",
+    name: "Jane Cooper",
+    classNo: 123123,
+    address: "부산시 해운대구",
+    class: "구미2반",
     email: "jane.cooper@example.com",
+    phone: "01000000000",
+    status: "leave",
     image:
       "https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=4&w=256&h=256&q=60",
   },
   {
-    name: "Jane Cooper",
     userId: 2,
-    title: "Regional Paradigm Technician",
-    department: "Optimization",
-    role: "Admin",
+    name: "Jane Cooper",
+    classNo: 123123,
+    address: "부산시 해운대구",
+    class: "구미2반",
     email: "jane.cooper@example.com",
+    phone: "01000000000",
+    status: "Studying",
     image:
       "https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=4&w=256&h=256&q=60",
   },
   {
-    name: "Jane Cooper",
     userId: 3,
-    title: "Regional Paradigm Technician",
-    department: "Optimization",
-    role: "Admin",
+    name: "Jane Cooper",
+    classNo: 123123,
+    address: "부산시 해운대구",
+    class: "구미2반",
     email: "jane.cooper@example.com",
+    phone: "01000000000",
+    status: "Studying",
     image:
       "https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=4&w=256&h=256&q=60",
   },
   {
-    name: "Jane Cooper",
     userId: 4,
-    title: "Regional Paradigm Technician",
-    department: "Optimization",
-    role: "Admin",
+    name: "Jane Cooper",
+    classNo: 123123,
+    address: "부산시 해운대구",
+    class: "구미2반",
     email: "jane.cooper@example.com",
+    phone: "01000000000",
+    status: "leave",
     image:
       "https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=4&w=256&h=256&q=60",
   },
   {
-    name: "Jane Cooper",
     userId: 5,
-    title: "Regional Paradigm Technician",
-    department: "Optimization",
-    role: "Admin",
+    name: "Jane Cooper",
+    classNo: 123123,
+    address: "부산시 해운대구",
+    class: "구미2반",
     email: "jane.cooper@example.com",
+    phone: "01000000000",
+    status: "leave",
     image:
       "https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=4&w=256&h=256&q=60",
   },
-  // More people...
+  {
+    userId: 6,
+    name: "Jane Cooper",
+    classNo: 123123,
+    address: "부산시 해운대구",
+    class: "구미2반",
+    email: "jane.cooper@example.com",
+    phone: "01000000000",
+    status: "Studying",
+    image:
+      "https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=4&w=256&h=256&q=60",
+  },
 ];
 
 interface Props {}
 
 function UserList({}: Props): ReactElement {
   const router = useRouter();
+  // MM보내기
+  function SendMM() {
+    alert("message");
+  }
   return (
-    <div className="flex flex-col mx-8">
+    <div className="flex flex-col mx-8 mt-2 text-center">
       <div className="-my-2 overflow-x-auto sm:-mx-6 lg:-mx-1">
         <div className="py-2 align-middle inline-block min-w-full sm:px-6 lg:px-8">
           <div className="shadow overflow-hidden border-b border-gray-200 sm:rounded-lg">
@@ -69,29 +94,44 @@ function UserList({}: Props): ReactElement {
                 <tr>
                   <th
                     scope="col"
-                    className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider"
+                    className="px-6 py-3 text-xs font-medium text-gray-500 uppercase tracking-wider"
                   >
                     Name
                   </th>
                   <th
                     scope="col"
-                    className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider"
+                    className="px-6 py-3 text-xs font-medium text-gray-500 uppercase tracking-wider"
                   >
-                    Title
+                    Class Info
                   </th>
                   <th
                     scope="col"
-                    className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider"
+                    className="px-6 py-3 text-xs font-medium text-gray-500 uppercase tracking-wider"
+                  >
+                    Address
+                  </th>
+                  <th
+                    scope="col"
+                    className="px-6 py-3 text-xs font-medium text-gray-500 uppercase tracking-wider"
+                  >
+                    Phone
+                  </th>
+                  <th
+                    scope="col"
+                    className="px-6 py-3 text-xs font-medium text-gray-500 uppercase tracking-wider"
                   >
                     Status
                   </th>
                   <th
                     scope="col"
-                    className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider"
+                    className="px-6 py-3 text-xs font-medium text-gray-500 uppercase tracking-wider"
                   >
-                    Role
+                    MM
                   </th>
-                  <th scope="col" className="relative px-6 py-3">
+                  <th
+                    scope="col"
+                    className="px-6 py-3 text-xs font-medium text-gray-500 uppercase tracking-wider"
+                  >
                     <span className="sr-only">Edit</span>
                   </th>
                 </tr>
@@ -115,28 +155,44 @@ function UserList({}: Props): ReactElement {
                             {person.name}
                           </div>
                           <div className="text-sm text-gray-500">
-                            {person.email}
+                            {person.classNo}
                           </div>
                         </div>
                       </div>
                     </td>
                     <td className="px-6 py-4 whitespace-nowrap">
                       <div className="text-sm text-gray-900">
-                        {person.title}
-                      </div>
-                      <div className="text-sm text-gray-500">
-                        {person.department}
+                        {person.class}
                       </div>
                     </td>
                     <td className="px-6 py-4 whitespace-nowrap">
-                      <span className="px-2 inline-flex text-xs leading-5 font-semibold rounded-full bg-green-100 text-green-800">
-                        Active
-                      </span>
+                      <div className="text-sm text-gray-900">
+                        {person.address}
+                      </div>
+                    </td>
+                    <td className="px-6 py-4 whitespace-nowrap">
+                      {person.phone}
                     </td>
                     <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-500">
-                      {person.role}
+                      {person.status === "leave" ? (
+                        <span className="px-2 inline-flex text-xs leading-5 font-semibold rounded-full bg-red-100 text-red-500">
+                          {person.status}
+                        </span>
+                      ) : (
+                        <span className="px-2 inline-flex text-xs leading-5 font-semibold rounded-full bg-green-100 text-green-800">
+                          {person.status}
+                        </span>
+                      )}
                     </td>
-                    <td className="px-6 py-4 whitespace-nowrap text-right text-sm font-medium">
+                    <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-500">
+                      <span
+                        className="px-2 inline-flex text-xs leading-5 font-semibold rounded-full bg-blue-400 text-black cursor-pointer"
+                        onClick={() => SendMM()}
+                      >
+                        MatterMost
+                      </span>
+                    </td>
+                    <td className="px-6 py-4 whitespace-nowrap text-sm font-medium">
                       <button
                         type="button"
                         className="text-indigo-600 hover:text-indigo-900"
@@ -147,7 +203,7 @@ function UserList({}: Props): ReactElement {
                           )
                         }
                       >
-                        Click me
+                        Detail
                       </button>
                     </td>
                   </tr>
