e6329d230bfd4f74575704cb263b4c722a7185bb
남근형 <magicom98@gmail.com>
2021-10-25 19:54:22 +0900

    makeProject

---
+++ backend/backend/src/main/kotlin/com/example/backend/jwt/JwtAuthenticationEntryPoint.java
@@ -0,0 +1,25 @@
+package com.example.backend.jwt;
+
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.web.AuthenticationEntryPoint;
+import org.springframework.stereotype.Component;
+
+// 유효한 자격증명을 제공하지 않고 접근하려 할때 401 Unuthorized 에러 리턴
+@Component
+public class JwtAuthenticationEntryPoint implements AuthenticationEntryPoint{
+
+    @Override
+    public void commence(HttpServletRequest request, HttpServletResponse response,
+                         AuthenticationException authException)
+            throws IOException, ServletException {
+        response.sendError(HttpServletResponse.SC_UNAUTHORIZED);
+    }
+
+}
