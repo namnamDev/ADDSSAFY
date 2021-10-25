f423ef573af66b303cfb461b771e5db68c17b977
Yeongwoo <77479615+ChoYeongwoo0322@users.noreply.github.com>
2021-10-25 15:42:39 +0900

    🔧 build : moment, react-countdown

--- FrontEnd/components/basic/ProNavbar.tsx
+++ FrontEnd/components/basic/ProNavbar.tsx
@@ -1,21 +1,47 @@
 import React, { ReactElement, Fragment } from "react";
 import { useRouter } from "next/router";
 import Image from "next/image";
-import { Transition, Menu } from "@headlessui/react";
+import { Transition, Menu, Popover } from "@headlessui/react";
+import { ChevronDownIcon } from "@heroicons/react/solid";
 
 function classNames(...classes: any) {
   return classes.filter(Boolean).join(" ");
 }
-
+const projects = [
+  {
+    No: 1,
+    name: "공통프로젝트",
+    description: "WebRTC, SNS, IOT",
+    href: "#",
+  },
+  {
+    No: 2,
+    name: "특화프로젝트",
+    description: "빅데이터(추천, 분산), 블록체인, IOT, AI(음성, 영상)",
+    href: "#",
+  },
+  {
+    No: 3,
+    name: "자율프로젝트",
+    description: "자율",
+    href: "#",
+  },
+];
 interface Props {}
 
 function ProNavbar({}: Props): ReactElement {
   const router = useRouter();
+  function gotoproject(projectNo: number) {
+    router.push(
+      `/ManageUserTeamBuilding/?projectNo=${projectNo}`,
+      "/ManageUserTeamBuilding"
+    );
+  }
   return (
     <div>
       <div className="relative bg-white">
         <div className="mx-auto px-4 sm:px-6">
-          <div className="flex justify-between items-center border-b-2 border-gray-200 py-4 md:justify-start md:space-x-10">
+          <div className="flex justify-between items-center border-b-2 border-gray-100 py-4 md:justify-start md:space-x-10">
             <div className="flex justify-start lg:w-0 lg:flex-1">
               <div
                 className="cursor-pointer"
@@ -42,12 +68,65 @@ function ProNavbar({}: Props): ReactElement {
             >
               서명지원금관리
             </div>
-            <div
-              className="text-base font-medium text-gray-500 hover:text-gray-900 cursor-pointer"
-              onClick={() => router.push(`/ManageUserTeamBuilding`)}
-            >
-              팀빌딩현황
-            </div>
+
+            <Popover.Group as="nav" className="hidden md:flex space-x-10">
+              <Popover className="relative">
+                {({ open }) => (
+                  <>
+                    <Popover.Button
+                      className={classNames(
+                        open ? "text-gray-900" : "text-gray-500",
+                        "group bg-white rounded-md inline-flex items-center text-base font-medium hover:text-gray-900 focus:outline-none focus:ring-2 focus:ring-offset-2"
+                      )}
+                    >
+                      <div className="text-base font-medium text-gray-500 hover:text-gray-900 cursor-pointer">
+                        팀빌딩현황
+                      </div>
+                      <ChevronDownIcon
+                        className={classNames(
+                          open ? "text-gray-600" : "text-gray-400",
+                          "ml-2 h-5 w-5 group-hover:text-gray-500"
+                        )}
+                        aria-hidden="true"
+                      />
+                    </Popover.Button>
+
+                    <Transition
+                      as={Fragment}
+                      enter="transition ease-out duration-200"
+                      enterFrom="opacity-0 translate-y-1"
+                      enterTo="opacity-100 translate-y-0"
+                      leave="transition ease-in duration-150"
+                      leaveFrom="opacity-100 translate-y-0"
+                      leaveTo="opacity-0 translate-y-1"
+                    >
+                      <Popover.Panel className="absolute z-10 -ml-4 mt-3 transform px-2 w-screen max-w-md sm:px-0 lg:ml-0 lg:left-1/2 lg:-translate-x-1/2">
+                        <div className="rounded-lg shadow-lg ring-1 ring-black ring-opacity-5 overflow-hidden">
+                          <div className="relative grid gap-6 bg-white px-5 py-6 sm:gap-8 sm:p-8">
+                            {projects.map((project) => (
+                              <div
+                                key={project.No}
+                                className="-m-3 p-3 flex items-start rounded-lg hover:bg-gray-50 cursor-pointer"
+                                onClick={() => gotoproject(project.No)}
+                              >
+                                <div className="ml-4">
+                                  <p className="text-base font-medium text-gray-900">
+                                    {project.name}
+                                  </p>
+                                  <p className="mt-1 text-sm text-gray-500">
+                                    {project.description}
+                                  </p>
+                                </div>
+                              </div>
+                            ))}
+                          </div>
+                        </div>
+                      </Popover.Panel>
+                    </Transition>
+                  </>
+                )}
+              </Popover>
+            </Popover.Group>
             <div
               className="text-base font-medium text-gray-500 hover:text-gray-900 cursor-pointer"
               onClick={() => router.push(`/Notice`)}
--- FrontEnd/pages/ManageUserTeamBuilding.tsx
+++ FrontEnd/pages/ManageUserTeamBuilding.tsx
@@ -1,12 +1,32 @@
 import React, { ReactElement } from "react";
 import ProNavbar from "../components/basic/ProNavbar";
+import { useRouter } from "next/router";
+import moment from "moment";
+import Countdown from "react-countdown";
 
 interface Props {}
 
 function ManageUserTeamBuilding({}: Props): ReactElement {
+  const router = useRouter();
+  const projectNo = router.query.projectNo;
+  // projectNo와 교육프로정보 같이보내서 기간 정보 가져오기
+
+  // 카운트다운 만들기
+  const nowTime = moment().format("YYYY-MM-DD HH:mm:ss");
+  const endTime = moment("2021-10-26 24:00:00");
+  var duration = moment.duration(endTime.diff(nowTime));
+  var rest = duration.asSeconds();
   return (
     <div>
       <ProNavbar />
+
+      <div className="text-center mt-5">
+        {rest > 0 ? (
+          <Countdown date={Date.now() + rest * 1000} />
+        ) : (
+          "경매가 종료되었습니다"
+        )}
+      </div>
     </div>
   );
 }
