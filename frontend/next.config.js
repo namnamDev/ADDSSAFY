5eb3672ad358769b848b6710fc9e6b1a2474e5aa
Yeongwoo <77479615+ChoYeongwoo0322@users.noreply.github.com>
2021-10-27 09:57:29 +0900

    merge request (park+cho)

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -1,22 +1,7 @@
 /** @type {import('next').NextConfig} */
 module.exports = {
-  images: {
-    domains: [
-      "loading.io",
-      "mblogthumb-phinf.pstatic.net",
-      "blog.hmgjournal.com",
-      "c4.wallpaperflare.com",
-      "img1.daumcdn.net",
-      "www.junggi.co.kr",
-      "highnoonsportfishing.com",
-      "www.williamsonrealty.com",
-      "w7.pngwing.com",
-      "cdn-icons-png.flaticon.com",
-      "media.vlpt.us",
-      "upload.wikimedia.org",
-      "previews.123rf.com",
-    ],
-  },
-
   reactStrictMode: true,
+  images: {
+    domains: ["images.unsplash.com", "tailwindui.com"],
+  },
 };
