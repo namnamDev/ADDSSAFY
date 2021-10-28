a8723c055352df588825bd5fc022de8a92329d65
teemollt <teemollt@gmail.com>
2021-10-28 16:02:51 +0900

    1

--- frontend/components/user/LoginModal.tsx
+++ frontend/components/user/LoginModal.tsx
@@ -19,7 +19,7 @@ function LoginModal({}: Props): ReactElement {
                 name="id"
                 type="email"
                 required
-                className="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-t-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm"
+                className="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-t-md focus:outline-none focus:ring-gray-900 focus:border-gray-600 focus:z-10 sm:text-sm"
                 placeholder="Mattermost ID"
               ></input>
             </div>
@@ -33,7 +33,7 @@ function LoginModal({}: Props): ReactElement {
                 type="password"
                 autoComplete="current-password"
                 required
-                className="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-b-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm"
+                className="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-b-md focus:outline-none focus:ring-gray-900 focus:border-gray-600 focus:z-10 sm:text-sm"
                 placeholder="Password"
               ></input>
             </div>
