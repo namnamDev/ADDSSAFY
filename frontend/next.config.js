65d1d5621dce0df868b9e50571442685d2d1da1d
Yeongwoo <77479615+ChoYeongwoo0322@users.noreply.github.com>
2021-11-02 12:22:41 +0900

    install

---
+++ frontend/next.config.js
@@ -0,0 +1,36 @@
+/** @type {import('next').NextConfig} */
+module.exports = {
+  async rewrites() {
+    return [
+      {
+        source: "/api/v4/:path*",
+        destination: `https://meeting.ssafy.com/api/v4/:path*`,
+      },
+      // backend
+      {
+        source: "/api/:path*",
+        destination: `http://k5d204.p.ssafy.io:8080/api/:path*`,
+      },
+    ];
+  },
+  reactStrictMode: true,
+  images: {
+    domains: [
+      "images.unsplash.com",
+      "tailwindui.com",
+      "loading.io",
+      "mblogthumb-phinf.pstatic.net",
+      "blog.hmgjournal.com",
+      "c4.wallpaperflare.com",
+      "img1.daumcdn.net",
+      "www.junggi.co.kr",
+      "highnoonsportfishing.com",
+      "www.williamsonrealty.com",
+      "w7.pngwing.com",
+      "cdn-icons-png.flaticon.com",
+      "media.vlpt.us",
+      "upload.wikimedia.org",
+      "previews.123rf.com",
+    ],
+  },
+};
