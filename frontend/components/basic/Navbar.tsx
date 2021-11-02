b5f15686b0b18e742bf85734c314ada956621381
Yeongwoo <77479615+ChoYeongwoo0322@users.noreply.github.com>
2021-11-02 14:59:27 +0900

    lodash.clonedeep 설치

--- frontend/components/basic/Navbar.tsx
+++ frontend/components/basic/Navbar.tsx
@@ -38,9 +38,9 @@ const projects = [
     description: "자율",
   },
 ];
-interface Props {}
+interface Props { }
 
-function Navbar({}: Props): ReactElement {
+function Navbar({ }: Props): ReactElement {
   const router = useRouter();
   // 로그아웃
   function logout() {
@@ -74,12 +74,7 @@ function Navbar({}: Props): ReactElement {
                 />
               </div>
             </div>
-            <div
-              className="text-base font-medium text-gray-500 hover:text-gray-900 cursor-pointer"
-              onClick={() => router.push(`/EduSigMain`)}
-            >
-              교육지원금 서류제출
-            </div>
+
             <Popover.Group as="nav" className="md:flex space-x-10">
               <Popover className="relative">
                 {({ open }) => (
@@ -138,69 +133,6 @@ function Navbar({}: Props): ReactElement {
                 )}
               </Popover>
             </Popover.Group>
-            <Popover.Group as="nav" className="hidden md:flex space-x-10">
-              <Popover className="relative">
-                {({ open }) => (
-                  <>
-                    <Popover.Button
-                      className={classNames(
-                        open ? "text-gray-900" : "text-gray-500",
-                        "group bg-white rounded-md inline-flex items-center text-base font-medium hover:text-gray-900 focus:outline-none focus:ring-2 focus:ring-offset-2"
-                      )}
-                    >
-                      <div className="text-base font-medium text-gray-500 hover:text-gray-900 cursor-pointer">
-                        자유게시판
-                      </div>
-                      <ChevronDownIcon
-                        className={classNames(
-                          open ? "text-gray-600" : "text-gray-400",
-                          "ml-2 h-5 w-5 group-hover:text-gray-500"
-                        )}
-                        aria-hidden="true"
-                      />
-                    </Popover.Button>
-
-                    <Transition
-                      as={Fragment}
-                      enter="transition ease-out duration-200"
-                      enterFrom="opacity-0 translate-y-1"
-                      enterTo="opacity-100 translate-y-0"
-                      leave="transition ease-in duration-150"
-                      leaveFrom="opacity-100 translate-y-0"
-                      leaveTo="opacity-0 translate-y-1"
-                    >
-                      <Popover.Panel className="absolute z-10 -ml-4 mt-3 transform px-2 w-screen max-w-md sm:px-0 lg:ml-0 lg:left-1/2 lg:-translate-x-1/2">
-                        <div className="rounded-lg shadow-lg ring-1 ring-black ring-opacity-5 overflow-hidden text-left">
-                          <div className="relative grid gap-6 bg-white px-5 py-6 sm:gap-8 sm:p-8">
-                            {boards.map((board) => (
-                              <div
-                                key={board.No}
-                                className="-m-3 p-3 flex items-start rounded-lg hover:bg-gray-50 cursor-pointer"
-                                onClick={() => gotoboard(board.url)}
-                              >
-                                <div className="ml-4">
-                                  <p className="text-base font-medium text-gray-900">
-                                    {board.name}
-                                  </p>
-                                  <p className="mt-1 text-sm text-gray-500">{board.description}</p>
-                                </div>
-                              </div>
-                            ))}
-                          </div>
-                        </div>
-                      </Popover.Panel>
-                    </Transition>
-                  </>
-                )}
-              </Popover>
-            </Popover.Group>
-            <div
-              className="text-base font-medium text-gray-500 hover:text-gray-900 cursor-pointer"
-              onClick={() => router.push(`/Notice`)}
-            >
-              공지사항
-            </div>
-
             <div className="hidden md:flex items-center justify-end md:flex-1 lg:w-0">
               <a
                 href="#"
