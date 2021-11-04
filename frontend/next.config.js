db0a6c4b3a333e58a2b8acf6dd69998a914226ab
박종한 <jonghan1983@naver.com>
2021-11-04 14:22:14 +0900

    배포

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -3,14 +3,13 @@ module.exports = {
   async rewrites() {
     return [
       {
-        source: "/api/v4/:path*",
+        source: "http://k5d204.p.ssafy.io/api/v4/:path*",
         destination: `https://meeting.ssafy.com/api/v4/:path*`,
       },
       // backend
       {
-        source: "/api/:path*",
-        // destination: `http://k5d204.p.ssafy.io:8080/api/:path*`,
-        destination: `http://k5d204.p.ssafy.io:8080/abcdedf`,
+        source: "http://k5d204.p.ssafy.io/api/:path*",
+        destination: `http://k5d204.p.ssafy.io:8080/api/:path*`,
       },
     ];
   },
