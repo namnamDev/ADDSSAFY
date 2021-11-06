73d7320ba11bfc1cbfde69f1465bef68b4bce9e5
박종한 <jonghan1983@naver.com>
2021-11-06 17:07:18 +0900

    배포

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -1,4 +1,4 @@
-** @type {import('next').NextConfig} */
+// ** @type {import('next').NextConfig} */
 const isProduction = process.env.NODE_ENV === "production";
 module.exports = {
   async rewrites() {
