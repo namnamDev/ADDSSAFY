2734d4aa1f6e1bf8f51accf0598cfb1fb879852e
Yeongwoo <77479615+ChoYeongwoo0322@users.noreply.github.com>
2021-11-07 23:47:48 +0900

    팀검색

---
+++ frontend/components/Team/SearchTeamList.tsx
@@ -0,0 +1,137 @@
+import React, { ReactElement, useState } from "react";
+import { useRouter } from "next/router";
+import Image from "next/image";
+import TeamCard from "./TeamCard";
+const people = [
+  {
+    userId: 1,
+    name: "Jane Cooper",
+    classNo: 123123,
+    address: "부산시 해운대구",
+    class: "구미2반",
+    email: "jane.cooper@example.com",
+    phone: "01000000000",
+    status: "leave",
+    image:
+      "https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=4&w=256&h=256&q=60",
+  },
+  {
+    userId: 2,
+    name: "Jane Cooper",
+    classNo: 123123,
+    address: "부산시 해운대구",
+    class: "구미2반",
+    email: "jane.cooper@example.com",
+    phone: "01000000000",
+    status: "Studying",
+    image:
+      "https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=4&w=256&h=256&q=60",
+  },
+  {
+    userId: 3,
+    name: "Jane Cooper",
+    classNo: 123123,
+    address: "부산시 해운대구",
+    class: "구미2반",
+    email: "jane.cooper@example.com",
+    phone: "01000000000",
+    status: "Studying",
+    image:
+      "https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=4&w=256&h=256&q=60",
+  },
+  {
+    userId: 4,
+    name: "Jane Cooper",
+    classNo: 123123,
+    address: "부산시 해운대구",
+    class: "구미2반",
+    email: "jane.cooper@example.com",
+    phone: "01000000000",
+    status: "leave",
+    image:
+      "https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=4&w=256&h=256&q=60",
+  },
+  {
+    userId: 5,
+    name: "Jane Cooper",
+    classNo: 123123,
+    address: "부산시 해운대구",
+    class: "구미2반",
+    email: "jane.cooper@example.com",
+    phone: "01000000000",
+    status: "leave",
+    image:
+      "https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=4&w=256&h=256&q=60",
+  },
+  {
+    userId: 6,
+    name: "Jane Cooper",
+    classNo: 123123,
+    address: "부산시 해운대구",
+    class: "구미2반",
+    email: "jane.cooper@example.com",
+    phone: "01000000000",
+    status: "Studying",
+    image:
+      "https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=4&w=256&h=256&q=60",
+  },
+];
+
+interface Props {
+  list: any;
+}
+
+function SearchTeamList({ list }: Props): ReactElement {
+  const router = useRouter();
+  // MM보내기
+  function SendMM() {
+    alert("message");
+  }
+  return (
+    <div className="flex flex-col mx-1 mt-2 text-center">
+      <div className="-my-2 overflow-x-auto">
+        <div className="py-2 align-middle inline-block min-w-full">
+          <div className="shadow overflow-hidden border-b border-gray-200 sm:rounded-lg">
+            <table className="min-w-full divide-y divide-gray-200">
+              <thead className="bg-gray-50">
+                <tr>
+                  <th
+                    scope="col"
+                    className="px-6 py-3 text-xs font-medium text-gray-500 uppercase tracking-wider"
+                  >
+                    Name
+                  </th>
+                  <th
+                    scope="col"
+                    className="px-6 py-3 text-xs font-medium text-gray-500 uppercase tracking-wider"
+                  >
+                    Class Info
+                  </th>
+                  <th
+                    scope="col"
+                    className="px-6 py-3 text-xs font-medium text-gray-500 uppercase tracking-wider"
+                  >
+                    Status
+                  </th>
+                  <th
+                    scope="col"
+                    className="px-6 py-3 text-xs font-medium text-gray-500 uppercase tracking-wider"
+                  >
+                    MM
+                  </th>
+                </tr>
+              </thead>
+              <tbody className="bg-white divide-y divide-gray-200">
+                {list.map((team: any) => (
+                  <TeamCard key={team.teamDto.teamPK} teamPK={team.teamDto.teamPK} />
+                ))}
+              </tbody>
+            </table>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default SearchTeamList;
